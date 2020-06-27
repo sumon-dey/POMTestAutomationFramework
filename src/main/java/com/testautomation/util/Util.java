6a82ae37114e83a974da6b5d525edd2b4c873378
Sumon <sdtechie22@gmail.com>
2020-06-27 22:22:26 +0530

    Added code for driving data from excel

--- src/main/java/com/testautomation/util/Util.java
+++ src/main/java/com/testautomation/util/Util.java
@@ -1,5 +1,12 @@
 package com.testautomation.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.WebElement;
 import com.testautomation.base.TestBase;
 
@@ -7,6 +14,9 @@ public class Util extends TestBase {
 
 	public static long PAGE_LOAD_TIMEOUT = 80;
 	public static long IMPLICIT_WAIT = 10;
+	public static String TESTDATA_SHEET_PATH = "./src/main/java/com/testautomation/testdata/TestData.xlsx";
+	static Workbook workbook;
+	static Sheet sheet;
 
 	public void switchToFrame(int index) {
 		driver.switchTo().frame(index);
@@ -18,6 +28,30 @@ public class Util extends TestBase {
 
 	public void switchToFrame(WebElement webElement) {
 		driver.switchTo().frame(webElement);
+	}
+
+	public static Object[][] getTestData(String sheetName) {
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			workbook = WorkbookFactory.create(fileInputStream);
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = workbook.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+			}
+		}
+		return data;
 	}
 
 }
--- src/test/java/com/testautomation/tests/SignInPageTest.java
+++ src/test/java/com/testautomation/tests/SignInPageTest.java
@@ -3,13 +3,17 @@ package com.testautomation.tests;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import com.testautomation.base.TestBase;
 import com.testautomation.pages.SignInPage;
+import com.testautomation.util.Util;
 
 public class SignInPageTest extends TestBase {
 
 	public SignInPage signInPage;
+	String sheetName = "DataSheet";
 
 	public SignInPageTest() {
 		super();
@@ -37,6 +41,18 @@ public class SignInPageTest extends TestBase {
 	public void loginFunctionalityTest() {
 		driver.get(prop.getProperty("signInPageUrl"));
 		signInPage.login(prop.getProperty("email"), prop.getProperty("password"));
+	}
+
+	@Test(priority = 3, dataProvider = "getSignInTestData")
+	public void loginFunctionalityTestWithMultipleData(String email, String password) {
+		driver.get(prop.getProperty("signInPageUrl"));
+		signInPage.login(email, password);
+	}
+
+	@DataProvider
+	public Object[][] getSignInTestData() {
+		Object[][] data = Util.getTestData(sheetName);
+		return data;
 	}
 
 }
